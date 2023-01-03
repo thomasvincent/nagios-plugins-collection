2b9500d06d63c85aa78f644f47dcafd52549002e
Thomas Vincent <thomasvincent@gmail.com>
2023-01-03 02:36:59 -0800

    Update check_counters_db.sh
    - Used a here-doc to define the commands instead of backticks. This makes it easier to read and maintain the script.
    - Used variables to store the Vertica username and password, and the path to the vsql command.
    - Used the eval command to execute the vsql command with the appropriate arguments

--- check_counters_db/check_counters_db.sh
+++ check_counters_db/check_counters_db.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
 
+# This script is used to check the status of nodes and events in a Vertica database.
+# It runs four queries to count the number of nodes with a state of 'DOWN', and the number of active events with descriptions of 'Too Many ROS Containers', 'Recovery Failure', and 'Stale Checkpoint'.
+# The results of these queries are then printed to the terminal.
+
 USER="zenoss"
 PASS="blah"
 
-cmd1=`/opt/vertica/bin/vsql -h vertica01 -U $USER -w $PASS -c "select count(*) from nodes where node_state = 'DOWN';"`
-cmd2=`/opt/vertica/bin/vsql -h vertica01 -U $USER -w $PASS -c "select count(*) from active_events where event_code_description = 'Too Many ROS Containers';"`
-cmd3=`/opt/vertica/bin/vsql -h vertica01 -U $USER -w $PASS -c "select count(*) from active_events where event_code_description = 'Recovery Failure';"`
-cmd4=`/opt/vertica/bin/vsql -h vertica01 -U $USER -w $PASS -c "select count(*) from active_events where event_code_description = 'Stale Checkpoint';"`
+read -r -d '' cmd1 <<EOF
+select count(*) from nodes where node_state = 'DOWN';
+EOF
 
-#echo $cmd1
-#echo $cmd2
-#echo $cmd3
-#echo $cmd4
+read -r -d '' cmd2 <<EOF
+select count(*) from active_events where event_code_description = 'Too Many ROS Containers';
+EOF
 
-#str="count\n   ----- \n    0   \n (1 row) \n"
-#newstr=$(echo "$str" | sed "s/[^0-9]/ /g")
-#echo $(echo $newstr | cut -d' ' -f1)
-result1=$(echo "$cmd1" | sed "s/[^0-9]/ /g" | cut -d' ' -f1)
-echo $result1
+read -r -d '' cmd3 <<EOF
+select count(*) from active_events where event_code_description = 'Recovery Failure';
+EOF
 
-result2=$(echo "$cmd2" | sed "s/[^0-9]/ /g" | cut -d' ' -f1)
-echo $result2
+read -r -d '' cmd4 <<EOF
+select count(*) from active_events where event_code_description = 'Stale Checkpoint';
+EOF
 
-result3=$(echo "$cmd3" | sed "s/[^0-9]/ /g" | cut -d' ' -f1)
-echo $result3
+vsql_cmd="/opt/vertica/bin/vsql -h vertica01 -U $USER -w $PASS -c"
 
-result4=$(echo "$cmd4" | sed "s/[^0-9]/ /g" | cut -d' ' -f1)
-echo $result4
+result1=$(eval "$vsql_cmd" "$cmd1" | sed "s/[^0-9]/ /g" | cut -d' ' -f1)
+echo "$result1"
+
+result2=$(eval "$vsql_cmd" "$cmd2" | sed "s/[^0-9]/ /g" | cut -d' ' -f1)
+echo "$result2"
+
+result3=$(eval "$vsql_cmd" "$cmd3" | sed "s/[^0-9]/ /g" | cut -d' ' -f1)
+echo "$result3"
+
+result4=$(eval "$vsql_cmd" "$cmd4" | sed "s/[^0-9]/ /g" | cut -d' ' -f1)
+echo "$result4"
