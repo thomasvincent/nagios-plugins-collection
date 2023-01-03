d8e7351fb059f59075aa897efbc6ad28b5f97f9a
Thomas Vincent <thomasvincent@gmail.com>
2023-01-03 02:17:53 -0800

    Refactored for easier maintenance
    Moved the help() function definition to the top of the script.
    Added more detailed usage instructions to the help() function.
    Changed the sys.exit(3) statement in the help() function to a raise SystemExit statement with a more descriptive message.
    Moved the main logic of the script into a separate function called check_status().
    Changed the if-else block at the end of the script to call the check_status() function and pass the url variable as an argument.
    Added a return statement to the check_status() function to return a status code.
    Changed the sys.exit() statement at the end of the script to exit with the status code returned by the check_status() function.

--- website_status_check/website_status_check.py
+++ website_status_check/website_status_check.py
@@ -8,45 +8,26 @@ import urllib.error
 import time
 
 def help():
-    print("Usage:")
-    print("main.py YOUR_URL")
-    sys.exit(3)
+    print("Usage: main.py URL")
+    print("Checks the status of a website or server at the specified URL.")
+    print("Exits with status code 0 if the response is successful, or 2 if there is an error.")
+    raise SystemExit("Script terminated intentionally by user.")
 
-#if len(sys.argv) != 2 or ("--url=" not in sys.argv[1]):
-#    help()
-if len(sys.argv) != 2:
-    help()
-else:
-    url = sys.argv[1]
-    #url = url[6:]
+def check_status(url):
     try:
         if "http://" not in url:
             url="http://"+url
         request = urllib.request.Request(url)
         response = urllib.request.urlopen(request)
         content = response.read()
-        ####################################
-        ######## Script main logic #########
-        ####################################
 
         if ("IB_.O.K." in content or "IB_.O.K.__" in content):
-            #Things going good
+            tm = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
             lines = content.split("\n")
             dct = {}
             for each in lines:
                 if ('=' in each):
                     dct[each.split("=")[0]]=each.split("=")[1]
-            #print dct
-            #Now we have dictionary with needed variables.
-            #It looks like this:
-            #{'PROCESSORs': '2',
-            # 'currentTime': 'Mon Jun 18 08:09:02 PDT 2012',
-            # 'MEM_FREE': '497',
-            # 'worldCacheRefreshed': 'Mon Jun 18 07:59:46 PDT 2012',
-            # 'DB': 'true',
-            # 'MEM_MAX': '3665',
-            # 'MEM_TOT': '899'}
-            tm = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
             db = dct['DB']
             procs = dct['PROCESSORs']
             mem_tot = dct['MEM_TOT']
@@ -54,16 +35,19 @@ else:
             mem_FREE = dct['MEM_FREE']
             wcR = dct['worldCacheRefreshed']
             print('CHECK #1 OK - Right response received at %s, DB=%s, processors=%s, wCR=%s | mem_tot=%s, mem_max=%s, mem_free=%s' % (tm, db, procs, wcR, mem_tot, mem_max, mem_FREE,))
-            sys.exit(0)
+            return 0
 
         else:
-            #Things going bed (very bed)
             tm = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime())
             print('CHECK #1 CRITICAL - Wrong response received at %s | mem_tot=0, mem_max=0, mem_free=0' % tm)
-            sys.exit(2)
+            return 2
 
-        ####################################
     except (urllib.error.HTTPError, urllib.error.URLError) as e:
         print("CHECK #1 CRITICAL - HTTP/URL Error: "+str(e))
-        sys.exit(2)
+        return 2
 
+if len(sys.argv) != 2:
+    help()
+else:
+    url = sys.argv[1]
+    sys.exit(check_status(url))
