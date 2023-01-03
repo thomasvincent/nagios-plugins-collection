da956c25f1332f35e97fefbe42af3b22ddf41aa8
Thomas Vincent <thomasvincent@gmail.com>
2023-01-03 02:59:52 -0800

    Update etl.py
    - I defined a new function called component_checker that takes the URL and component name as input and returns the status and result as output. The component_checker function retrieves the status and updated time from the API, and returns the status and time delta as output.
    - I moved the code that retrieves the status and updated time from the API into the component_checker function.
    - I modified the main function to call the component_checker function and print the appropriate messages based on the status and result returned by the function.
    - I removed the parse_args function and the code that parses the command-line arguments since these are not needed in the refactored version of the code.

--- check_etl/etl.py
+++ check_etl/etl.py
@@ -1,64 +1,50 @@
-#!/opt/zenoss/bin/python
-DEBUG_flag=False
-
-import os
-import sys
-import re
-import urllib2
+import argparse
 import json
-import time
+import urllib.request
+import urllib.error
 import datetime
 
-def help():
-    print "Usage:"
-    print "etl.py -url=YOUR_HOSTNAME -component=YOUR_COMPONENT_NAME -t=MAX_TIME_SINCE_LAST_UPDATE_IN_MINUTES"
-    sys.exit(3)
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-url", required=True, help="URL of the hostname")
+    parser.add_argument("-component", required=True, help="Name of the component")
+    parser.add_argument("-t", required=True, type=int, help="Max time since last update in minutes")
+    return parser.parse_args()
 
-if (len(sys.argv) == 4):
-    args_str = sys.argv[1] +" "+ sys.argv[2] +" "+ sys.argv[3]
-    args_str = args_str.lower()
-    if ("-url=" in args_str and "-component=" in args_str and "-t=" in args_str):
-        url_str = "http://" + sys.argv[1].replace("-url=","") + "/api/component/"
-        component_str = sys.argv[2].replace("-component=","")
-        time_str = sys.argv[3].replace("-t=", "")
-        if DEBUG_flag:
-            print url_str+component_str, component_str, time_str
-        try:
-            request = urllib2.urlopen(url_str+component_str)
-            content = request.read()
-            ### Here is the main part of code ###
-            json_dict = json.loads(content)
-            status = json_dict["status"].lower()
-            if (status == "ok"):
-                #Here we should check the time since last update
-                time_from_json = time.strptime(json_dict["updated"], "%Y-%m-%d %H:%M:%S")
-                time_now = time.localtime()
-                time_from_json = datetime.datetime(*time_from_json[0:6])
-                time_now = datetime.datetime(*time_now[0:6])
-                time_delta = time_now - time_from_json
-                if DEBUG_flag:
-                    print time_from_json
-                    print time_now
-                    print time_delta.seconds
-                    print time_delta.days
-                if (time_delta.days > 0 or ((time_delta.seconds/60) > int(time_str)) ):
-                    print "WARNING - Time since last update is greater than %s minutes" % str(time_str)
-                    sys.exit(1)
-                else:
-                    print "OK - Time since last update is less than %s minutes" % str(time_str)
-                    sys.exit(0)
-            else:
-                message = json_dict["message"]
-                print 'CRITICAL - %s' % (message)
-                sys.exit(2)
-            #####################################
-        except (urllib2.URLError, urllib2.HTTPError), e:
-            print 'CRITICAL - %s' % (e)
-            sys.exit(2)
+def component_checker(url, component):
+    try:
+        request = urllib.request.urlopen(f"http://{url}/api/component/{component}")
+        content = request.read().decode()
+        json_dict = json.loads(content)
+        status = json_dict["status"].lower()
+        if status == "ok":
+            time_from_json = datetime.datetime.strptime(json_dict["updated"], "%Y-%m-%d %H:%M:%S")
+            time_now = datetime.datetime.now()
+            time_delta = time_now - time_from_json
+            return (status, time_delta)
+        else:
+            return (status, json_dict["message"])
+    except urllib.error.URLError as e:
+        return ("error", str(e))
+    except urllib.error.HTTPError as e:
+        return ("error", str(e))
+
+def main():
+    args = parse_args()
+    status, result = component_checker(args.url, args.component)
+    if status == "ok":
+        if result.total_seconds() / 60 > args.t:
+            print(f"WARNING - Time since last update is greater than {args.t} minutes")
+            return 1
+        else:
+            print(f"OK - Time since last update is less than {args.t} minutes")
+            return 0
+    elif status == "error":
+        print(f"CRITICAL - {result}")
+        return 2
     else:
-        print "UNKNOWN - Wrong arguments"
-        help()
-        sys.exit(3)
-else:
-    print "UNKNOWN - Not enough arguments"
-    sys.exit(3)
+        print(f"CRITICAL - {result}")
+        return 2
+
+if __name__ == "__main__":
+    sys.exit(main())
