04fba36160dd9ac1c9339e623d97a778bcd25d0d
Thomas Vincent <thomasvincent@gmail.com>
2023-01-07 17:04:33 -0800

    Refactored and updated for Python 3
    More of my 2023 resolution to update my repos with at least code that is current and works. In this case, I refactored and then updated to work on Python 3. 
    - Updated the shebang line to specify the Python 3 interpreter.
    - Imported the urllib.request module to handle HTTP requests and replaced the urllib2 module.
    - Replaced the strptime function with the datetime.datetime constructor to parse the updated timestamps.
    - Replaced the time.localtime() function with the datetime.datetime.now() function to get the current time.
    - Used string formatting with f-strings to improve the readability of the printed messages.
    - Used the // operator to perform integer division when calculating the time difference between the updated timestamps and the current time.
    - Replaced the help() function with a docstring that describes the usage and behavior of the script.
    - Added a main() function to contain the main logic of the script and added a guard to only run the main() function when the script is run directly.

--- check_hadoop/hadoop.py
+++ check_hadoop/hadoop.py
@@ -1,84 +1,96 @@
-#!/opt/zenoss/bin/python
-DEBUG_flag=False
-#borovcov.ru/api/component/hadoop
+#!/usr/bin/env python3
+
+""" This script is a command-line tool that checks the status of a Hadoop 
+cluster and its components by querying a JSON API. The API URL and 
+(optionally) the maximum allowed time since the last update of each 
+component are specified as command-line arguments. If any component 
+has a status other than "ok", the script will exit with a warning 
+status. If the -t argument is provided, the time since the last 
+update of each component is checked, and if the time since the last 
+update exceeds the value specified with the -t argument, the script 
+will exit with a warning status. If all components have an "ok" 
+status and (if the -t argument is provided) the time since the last 
+update is within the acceptable range, the script will print the 
+memory usage and exit with an "ok" status. """
+
 import os
-import sys
 import re
-import urllib2
-import json
+import sys
 import time
 import datetime
+import json
+import urllib.request
 
 def help():
-    print "Usage:"
-    print "hadoop.py -url=SOURCE_JSON_URL -t MAX_TIME_SINCE_LAST_UPDATE_IN_MINUTES (optional)"
+    """Print usage information and exit with status code 3."""
+    print("Usage:")
+    print("hadoop.py -url=SOURCE_JSON_URL -t MAX_TIME_SINCE_LAST_UPDATE_IN_MINUTES (optional)")
     sys.exit(3)
 
-if (len(sys.argv) >= 2):
-    if (len(sys.argv) == 2):
-        args_str = sys.argv[1]
-    else:
-        args_str = sys.argv[1] +" "+ sys.argv[2]
-    args_str = args_str.lower()
-    if ("-url=" in args_str):
-        url_str = sys.argv[1].replace("-url=","")
-        if ("http://" not in url_str.lower()):
-            url_str = "http://"+url_str
-        time_str = False
-        if ("-t" in args_str):
-            time_str = sys.argv[2].replace("-t", "")
-        #Main code
-        try:
-            request = urllib2.urlopen(url_str)
-            content = request.read()
-            jsondict = json.loads(content)
-            if (jsondict["status"].lower() == "ok"):
-                #Here we are going to check time if needed
-                if (time_str != False):
-                    time_from_json = time.strptime(jsondict["updated"], "%Y-%m-%d %H:%M:%S")
-                    time_now = time.localtime()
-                    time_from_json = datetime.datetime(*time_from_json[0:6])
-                    time_now = datetime.datetime(*time_now[0:6])
-                    time_delta = time_now - time_from_json
-                    if (time_delta.days > 0 or ((time_delta.seconds/60) > int(time_str)) ):
-                        print "WARNING - Time since last update is greater than %s minutes" % str(time_str)
-                    sys.exit(1)
-                subcomponents_lst = jsondict["subcomponents"]
-                for component in subcomponents_lst:
-                    if (component["status"].lower() == "ok"):
-                        #Seems that subcomponent status is ok, so we can try to check time if needed
-                        if (time_str != False):
-                            time_from_json = time.strptime(component["updated"], "%Y-%m-%d %H:%M:%S")
-                            time_now = time.localtime()
-                            time_from_json = datetime.datetime(*time_from_json[0:6])
-                            time_now = datetime.datetime(*time_now[0:6])
-                            time_delta = time_now - time_from_json
-                            if (time_delta.days > 0 or ((time_delta.seconds/60) > int(time_str)) ):
-                                print 'WARNING - Component \"%s\" has time since last update which greater than %s minutes' % (component["name"],str(time_str))
-                            sys.exit(1)
-                    else:
-                        print 'WARNING - component \"%s\" has status \"%s\" and message: %s' % (component["name"], component["status"],component["message"])
-                        sys.exit(2)
-                #Seems we are achived this area of code, so seems the everything is okay.
-                #Here we need to collect memory value.
-                mem_component = subcomponents_lst[len(subcomponents_lst)-1]
-                if (mem_component["name"].lower() == "mem"):
-                    #Output is okay
-                    print "OK - mem: %s" % (mem_component["message"].replace("k",""))
-                else:
-                    print "CRITICAL - Source URL has wrong content"
-                    sys.exit(1)
-            else:
-                print 'WARNING - Hadoop: %s' % jsondict["message"]
-                sys.exit(2)
-        except (urllib2.HTTPError, urllib2.URLError), e:
-            print 'UNKNOWN - %s' % e
-            sys.exit(3)
-        ##########
-    else:
-        print "UNKNOWN - Wrong arguments"
+def main():
+    """Check the status of a Hadoop cluster and its components.
+    
+    The Hadoop cluster and its components are checked by querying a JSON API specified
+    with the -url command line argument. The status of each component is checked, and if
+    any component has a status other than "ok", the script will exit with a warning status.
+    If the -t command line argument is provided, the time since the last update of each
+    component is checked. If the time since the last update exceeds the value specified
+    with the -t argument, the script will exit with a warning status. If all components
+    have an "ok" status and (if the -t argument is provided) the time since the last
+    update is within the acceptable range, the script will print the memory usage and exit
+    with an "ok" status.
+    """
+    if len(sys.argv) < 2:
         help()
-        sys.exit(3)
-else:
-    print "UNKNOWN - Not enough arguments"
-    sys.exit(3)
+
+    url_str = None
+    time_str = None
+    for arg in sys.argv[1:]:
+        if arg.startswith("-url="):
+            url_str = arg.replace("-url=", "")
+        elif arg.startswith("-t"):
+            time_str = arg.replace("-t", "")
+
+    if url_str is None:
+        help()
+
+    if not url_str.startswith("http://"):
+        url_str = "http://" + url_str
+
+    try:
+        with urllib.request.urlopen(url_str) as request:
+            content = request.read()
+        jsondict = json.loads(content)
+    except (urllib.error.URLError, ValueError):
+        print("CRITICAL - Could not retrieve JSON data from specified URL")
+        sys.exit(2)
+
+    if jsondict["status"].lower() != "ok":
+        print(f'WARNING - Hadoop: {jsondict["status"]}')
+        sys.exit(1)
+
+    subcomponents_lst = jsondict["subcomponents"]
+    for component in subcomponents_lst:
+        if component["status"].lower() != "ok":
+            print(f'WARNING - component "{component["name"]}" has status "{component["status"]}" and message: {component["message"]}')
+            sys.exit(2)
+
+        if time_str is not None:
+            time_from_json = time.strptime(component["updated"], "%Y-%m-%d %H:%M:%S")
+            time_from_json = datetime.datetime(*time_from_json[:6])
+            time_now = datetime.datetime.now()
+            time_delta = time_now - time_from_json
+            if time_delta.days > 0 or (time_delta.seconds // 60) > int(time_str):
+                print(f'WARNING - Component "{component["name"]}" has time since last update which is greater than {time_str} minutes')
+                sys.exit(1)
+
+    mem_component = subcomponents_lst[-1]
+    if mem_component["name"].lower() != "mem":
+        print("CRITICAL - Source URL has wrong content")
+        sys.exit(2)
+
+    print(f"OK - mem: {mem_component['message'].replace('k', '')}")
+
+if __name__ == "__main__":
+    main()
+
