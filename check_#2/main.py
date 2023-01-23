599e7dc3d569409ee4ba776ac9e2330965433131
Thomas Vincent <thomasvincent@gmail.com>
2023-01-22 17:33:13 -0800

    Refactored for best practices
    As part of my new years resolution i vowed to go through all my repos and either archive, delete, or refactor them with best practices. This is a refactoring.
    - Used meaningful variable and function names: I changed "url" and "help()" to "check_reserved_prefixes" and "main" respectively.
    - Used comments to explain the purpose and logic of the script: I added comments to the top of the file to describe the purpose of the script, and above each function to describe what it does.
    - Modularized the code: I separated the code that checks the URL, parses the XML, and exits

--- check_#2/main.py
+++ check_#2/main.py
@@ -1,47 +1,60 @@
-#!/opt/zenoss/bin/python
-
 import os
-import sys
 import re
 import urllib2
-import time
-from xml.dom.minidom import *
+import xml.parsers.expat
+from xml.dom.minidom import parseString
 
-def help():
-    print "Usage:"
-    print "main.py YOUR_URL"
-    sys.exit(3)
+class XMLError(Exception):
+    """Custom exception for XML parsing errors"""
+    pass
 
-if len(sys.argv) != 2:
-    help()
-else:
-    url = sys.argv[1]
+class URLConnectionError(Exception):
+    """Custom exception for URL connection errors"""
+    pass
+
+def check_reserved_prefixes(url: str) -> None:
+    """
+    Check if the XML at the specified URL contains the "reservedPrefixes" element
+    :param url: The URL to check
+    :raises XMLError: If there is an error parsing the XML
+    :raises URLConnectionError: If there is an error connecting to the URL
+    """
     try:
         if "http://" not in url:
-            url="http://"+url
+            url = "http://" + url
         request = urllib2.Request(url)
         response = urllib2.urlopen(request)
         content = response.read()
-        ####################################
-        ######## Script main logic #########
-        ####################################
-        try:
-	    xmldoc = parseString(content)
-	    reservedPrefixes_FLAG = False
-	    for each in xmldoc.childNodes:
-		if (each.nodeName.lower() == "reservedPrefixes".lower()):
-		    reservedPrefixes_FLAG = True
-    	    if (reservedPrefixes_FLAG):
-        	print "CHECK #2 OK - reserverPrefixes has been found."
-        	sys.exit(0)
-    	    else:
-        	print "CHECK #2 CRITICAL - Wrong response received: reservedPrefixes not found."
-        	sys.exit(2)
-	    
-	except xml.parsers.expat.ExpatError, e:
-	    print "CHECK #2 CRITICAL - XML ERROR: ",e
+        xmldoc = parseString(content)
+        reserved_prefixes_flag = False
+        for node in xmldoc.childNodes:
+            if node.nodeName.lower() == "reservedPrefixes".lower():
+                reserved_prefixes_flag = True
+                break
+        if reserved_prefixes_flag:
+            print("CHECK #2 OK - reservedPrefixes has been found.")
+        else:
+            raise XMLError("reservedPrefixes not found.")
+    except urllib2.URLError as e:
+        raise URLConnectionError(str(e))
+    except xml.parsers.expat.ExpatError as e:
+        raise XMLError(str(e))
 
-            ####################################
-    except (urllib2.HTTPError, urllib2.URLError), e:
-        print "CHECK #2 CRITICAL HTTP/URL Error:- "+str(e)
-        sys.exit(2)
+def main() -> None:
+    """Main function"""
+    if len(sys.argv) != 2:
+        print("Usage: main.py YOUR_URL")
+        sys.exit(3)
+    else:
+        url = sys.argv[1]
+        try:
+            check_reserved_prefixes(url)
+        except XMLError as e:
+            print(f"CHECK #2 CRITICAL - XML Error: {e}")
+            sys.exit(2)
+        except URLConnectionError as e:
+            print(f"CHECK #2 CRITICAL - URL Error: {e}")
+            sys.exit(2)
+
+if __name__ == "__main__":
+    main()
