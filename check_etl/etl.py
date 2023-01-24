f3feac1da67e79cf383d879bb64140f1675e39ef
Thomas Vincent <thomasvincent@gmail.com>
2023-01-24 01:36:59 -0800

    Refactored and modernized
    - Added docstring
    - Took advantage of the newer features in 3.8

--- check_etl/etl.py
+++ check_etl/etl.py
@@ -3,21 +3,35 @@ import json
 import urllib.request
 import urllib.error
 import datetime
+from typing import Tuple
 
-def parse_args():
+def parse_args() -> argparse.Namespace:
+    """
+    Parses command line arguments and returns them as a Namespace object
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("-url", required=True, help="URL of the hostname")
     parser.add_argument("-component", required=True, help="Name of the component")
     parser.add_argument("-t", required=True, type=int, help="Max time since last update in minutes")
     return parser.parse_args()
 
-def component_checker(url, component):
+def component_checker(url: str, component: str) -> Tuple[str, Union[datetime.timedelta, str]]:
+    """
+    Check the status of a component by querying the JSON API.
+
+    :param url: URL of the hostname
+    :param component: Name of the component
+    :return: a tuple of (status, result) where status is one of ('ok', 'error')
+    and result is a timedelta object representing the time since last update for 'ok' status 
+    or a string representing the error message for 'error' status
+    """
     try:
         request = urllib.request.urlopen(f"http://{url}/api/component/{component}")
         content = request.read().decode()
         json_dict = json.loads(content)
         status = json_dict["status"].lower()
         if status == "ok":
+            time_from_json = datetime.datetime.strptime
             time_from_json = datetime.datetime.strptime(json_dict["updated"], "%Y-%m-%d %H:%M:%S")
             time_now = datetime.datetime.now()
             time_delta = time_now - time_from_json
@@ -29,7 +43,10 @@ def component_checker(url, component):
     except urllib.error.HTTPError as e:
         return ("error", str(e))
 
-def main():
+def main() -> int:
+    """
+    The main function of the script.
+    """
     args = parse_args()
     status, result = component_checker(args.url, args.component)
     if status == "ok":
