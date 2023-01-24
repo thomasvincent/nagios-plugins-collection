a16eace2173a45d2a68fd07e4acfa8c15a654177
Thomas Vincent <thomasvincent@gmail.com>
2023-01-24 01:46:38 -0800

    Update test_etl.py

--- check_etl/test_etl.py
+++ check_etl/test_etl.py
@@ -1,18 +1,30 @@
-import pytest
-from component_checker import component_checker
+import unittest
+from unittest.mock import patch
+import argparse
+import json
+import urllib.request
+import urllib.error
+import datetime
+from component_checker import main, parse_args, component_checker
 
-def test_component_checker():
-    # Test with a valid URL and component name
-    status, result = component_checker("example.com", "component1")
-    assert status == "ok"
-    assert isinstance(result, datetime.timedelta)
+class TestMain(unittest.TestCase):
+    def test_main_ok(self):
+        with patch('component_checker.component_checker') as mocked_component_checker:
+            mocked_component_checker.return_value = ('ok', datetime.timedelta(minutes=10))
+            args = parse_args(['-url=example.com', '-component=component1', '-t=30'])
+            result = main(args)
+            self.assertEqual(result, 0)
 
-    # Test with an invalid URL
-    status, result = component_checker("invalid.com", "component1")
-    assert status == "error"
-    assert isinstance(result, str)
+    def test_main_warning(self):
+        with patch('component_checker.component_checker') as mocked_component_checker:
+            mocked_component_checker.return_value = ('ok', datetime.timedelta(minutes=40))
+            args = parse_args(['-url=example.com', '-component=component1', '-t=30'])
+            result = main(args)
+            self.assertEqual(result, 1)
 
-    # Test with an invalid component name
-    status, result = component_checker("example.com", "invalid")
-    assert status == "error"
-    assert isinstance(result, str)
+    def test_main_error(self):
+        with patch('component_checker.component_checker') as mocked_component_checker:
+            mocked_component_checker.return_value = ('error', 'something went wrong')
+            args = parse_args(['-url=example.com', '-component=component1', '-t=30'])
+            result = main(args)
+            self.assertEqual(result, 2)
