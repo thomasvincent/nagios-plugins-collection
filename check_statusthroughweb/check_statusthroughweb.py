0c008e7e115ca83bc97dd207e2c4c2e7a7edf422
Thomas Vincent <thomasvincent@gmail.com>
2023-03-05 21:09:15 -0800

    Update check_statusthroughweb.py
    I created a ServiceChecker class to encapsulate the service checking logic and made use of constants to avoid hardcoding values. I also separated the get_content, get_services_status, and get_ip_address functions to make the code more modular and easier to read. The run function now calls these functions to check the services, get the IP address, and print the appropriate message. Finally, I added error handling for urlopen calls to avoid unhandled exceptions.

--- check_statusthroughweb/check_statusthroughweb.py
+++ check_statusthroughweb/check_statusthroughweb.py
@@ -1,85 +1,82 @@
-#!/opt/zenoss/bin/python
+#!/usr/bin/env python3
 
-import os
 import sys
 import re
-import urllib2
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
 
-def help():
-    print "Usage:"
-    #print "check_statusthroughweb.py --url=YOUR_URL"
-    print "check_statusthroughweb.py YOUR_URL"
-    sys.exit(3)
+class ServiceChecker:
+    # Define constants
+    PHP = 'PHP'
+    MYSQL = 'MySQL'
+    MEMCACHE = 'MemCache'
+    CDN = 'CDN'
+    URL_PREFIX = 'http://'
+    FAILED_STATUS_MSG = '{} service down'
 
-#if len(sys.argv) != 2 or ("--url=" not in sys.argv[1]):
-#    help()
-if len(sys.argv) != 2:
-    help()
-else:
-    url = sys.argv[1]
-    #url = url[6:]
-    try:
-        if "http://" not in url:
-            url="http://"+url
-        request = urllib2.Request(url)
-        response = urllib2.urlopen(request)
-        content = response.read()
-        ####################################
-        php_flag = False
-        mysql_flag = False
-        memcache_flag = False
-        cdn_flag = False
-        content=content.lower()
-        failed_count = 0
-        if "php up" in content:
-            php_flag = True
-        else:
-            failed_count = failed_count + 1
+    def __init__(self, url):
+        self.url = url
 
-        if "mysql up" in content:
-            mysql_flag = True
-        else:
-            failed_count = failed_count + 1
+    def run(self):
+        content = self.get_content()
+        services_status = self.get_services_status(content)
+        ip = self.get_ip_address(content)
 
-        if "memcache up" in content:
-            memcache_flag = True
-        else:
-            failed_count = failed_count + 1
-
-        if "cdn up" in content:
-            cdn_flag = True
-        else:
-            failed_count = failed_count + 1
-
-        ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', content )[0]
-
-        if php_flag and mysql_flag and memcache_flag and cdn_flag and failed_count == 0:
-            print "OK - PHP, MySQL and MemCache services are ok at server "+str(ip)
+        # If all services are up, print "OK" and exit with code 0
+        if all(services_status.values()):
+            print(f"OK - {', '.join(services_status.keys())} services are up at server {ip}")
             sys.exit(0)
 
-        if failed_count>0:
-            if failed_count<4:
-                #Print warning with failure services
-                output = "WARNING - "
-                if not php_flag:
-                    output = output + "PHP service down, "
-                if not mysql_flag:
-                    output = output + "MySQL service down, "
-                if not memcache_flag:
-                    output = output + "MemCache service down, "
-                if not cdn_flag:
-                    output = output + "CDN service down, "
+        # If some services are down, print a warning and exit with code 1
+        failed_services = [s for s, status in services_status.items() if not status]
+        if failed_services:
+            failed_services_str = ', '.join(self.FAILED_STATUS_MSG.format(s) for s in failed_services)
+            print(f"WARNING - {failed_services_str} at server {ip}")
+            sys.exit(1)
 
-                output = output[:-2] + " at server "+str(ip)
-                print output
-                sys.exit(1)
-
-            if failed_count==4:
-                #Print critical failure
-                print "CRITICAL - Server failure at address "+str(ip)
-                sys.exit(2)
-
-        ####################################
-    except (urllib2.HTTPError, urllib2.URLError), e:
-        print "CRITICAL - "+str(e)
+        # If all services are down, print a critical message and exit with code 2
+        print(f"CRITICAL - Server failure at address {ip}")
         sys.exit(2)
+
+    def get_content(self):
+        try:
+            response = urlopen(self.url)
+            content = response.read().decode().lower()
+            return content
+        except (HTTPError, URLError) as e:
+            print(f"CRITICAL - {e}")
+            sys.exit(2)
+
+    def get_services_status(self, content):
+        services = {
+            self.PHP: 'php up' in content,
+            self.MYSQL: 'mysql up' in content,
+            self.MEMCACHE: 'memcache up' in content,
+            self.CDN: 'cdn up' in content
+        }
+        return services
+
+    def get_ip_address(self, content):
+        match = re.search(r'[0-9]+(?:\.[0-9]+){3}', content)
+        if match:
+            return match.group(0)
+        return 'unknown'
+
+def help():
+    # Help function to display usage
+    print("Usage:")
+    print(f"{sys.argv[0]} YOUR_URL")
+    sys.exit(3)
+
+# Check if the correct number of arguments is passed, else display usage and exit
+if len(sys.argv) != 2:
+    help()
+
+url = sys.argv[1]
+
+# Prepend "http://" to the URL if it's not already present
+if not url.startswith(ServiceChecker.URL_PREFIX):
+    url = ServiceChecker.URL_PREFIX + url
+
+checker = ServiceChecker(url)
+checker.run()
