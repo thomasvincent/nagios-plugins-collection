9521599b3760aebd263cdf6db6fd1ccc0b99594b
Thomas Vincent <thomasvincent@gmail.com>
2023-03-05 21:07:05 -0800

    Update check_scribe.sh

--- check_scribe/check_scribe.sh
+++ check_scribe/check_scribe.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-#USAGE
+# USAGE
 # check_scribe SSH_USERNAME SSH_HOST
+# This script is used to check the status of the Scribe daemon on a remote server via SSH.
 
 # Nagios exit codes
 nagios_ok=0
@@ -14,25 +15,23 @@ scribe_ok="ALIVE"
 scribe_warning="WARNING"
 scribe_critical="[(STARTING)(STOPPING)(STOPPED)(DEAD)(Failed to get status)]"
 
+# Build SSH command to run scribe_ctrl status on remote server
 admin="ssh $1@$2 /usr/sbin/scribe_ctrl status"
-echo $admin
+echo "$admin"
 
-rval=`$admin`
+# Execute SSH command and capture output in variable
+rval=$(eval "$admin")
 
+# Check output for Scribe service status and return appropriate Nagios exit code
 if [[ "$rval" =~ "$scribe_ok" ]]; then
-        echo "OK"
-        exit $nagios_ok
-
+    echo "OK"
+    exit $nagios_ok
 elif [[ "$rval" =~ "$scribe_warning" ]]; then
-        echo "WARNING - $rval"
-
+    echo "WARNING - $rval"
 elif [[ "$rval" =~ "$scribe_critical" ]]; then
-        echo "CRITICAL"
-        exit $nagios_critical
-
+    echo "CRITICAL"
+    exit $nagios_critical
 else
-        echo "UNKNOWN - Scribe dameon is in an unknown state: $rval"
-        exit $nagios_unknown
+    echo "UNKNOWN - Scribe daemon is in an unknown state: $rval"
+    exit $nagios_unknown
 fi
-
-
