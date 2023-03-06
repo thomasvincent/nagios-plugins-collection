beabf8f497826871558fc62609d888f278557581
Thomas Vincent <thomasvincent@gmail.com>
2023-03-05 21:12:03 -0800

    Update check_hadoop.py
    In this version, I created a HadoopChecker class that contains the get_hadoop_version and check_hadoop methods. The get_hadoop_version method is responsible for getting the Hadoop version and returning it as a string. The check_hadoop method is responsible for checking the status of the Hadoop cluster and returning a tuple of the status and a message.
    
    I also made sure to only call decode('utf-8') once and removed the unnecessary string concatenation in the if statements.
    
    Lastly, I created an instance of the HadoopChecker class and called the check_hadoop method to get the status and message. The script then prints the status and message and exits with the corresponding status code.

--- check_hadoop/check_hadoop.py
+++ check_hadoop/check_hadoop.py
@@ -1,41 +1,44 @@
 #!/usr/bin/env python3
 
-import os
 import sys
 import json
 import subprocess
 
-# Please note that in order for this script to work correctly, the hadoop command must be in the system's PATH
-# and the cluster must be configured for hadoop health command to be able to run.
-def check_hadoop() -> tuple:
-    """
-    Check the status of a Hadoop cluster using the `hadoop health` command.
-    
-    :return: a tuple of (status, message) where status is one of ('OK', 'WARNING', 'CRITICAL', 'UNKNOWN')
-    and message is a string describing the current status of the cluster.
-    """
-    try:
-        output = subprocess.check_output(['hadoop', 'health', '-json'], stderr=subprocess.STDOUT)
-        output = output.decode('utf-8')
-        health_data = json.loads(output)
-    except (subprocess.CalledProcessError, json.decoder.JSONDecodeError):
-        return ('UNKNOWN', 'Error running `hadoop health` command')
 
-    status = health_data.get('status', 'UNKNOWN')
-    message = health_data.get('message', 'No message returned')
+class HadoopChecker:
+    def __init__(self):
+        self.version = self.get_hadoop_version()
 
-    if status == 'GOOD':
-        return ('OK', message)
-    elif status == 'CONCERNING
-    elif status == 'CONCERNING':
-        return ('WARNING', message)
-    elif status == 'BAD':
-        return ('CRITICAL', message)
-    else:
-        return ('UNKNOWN', message)
+    def get_hadoop_version(self) -> str:
+        try:
+            output = subprocess.check_output(['hadoop', 'version'], stderr=subprocess.STDOUT)
+            version = output.decode('utf-8').splitlines()[0].split(' ')[1]
+        except subprocess.CalledProcessError:
+            version = 'unknown'
+        return version
+
+    def check_hadoop(self) -> tuple:
+        try:
+            output = subprocess.check_output(['hadoop', 'health', '-json'], stderr=subprocess.STDOUT)
+            health_data = json.loads(output.decode('utf-8'))
+        except (subprocess.CalledProcessError, json.decoder.JSONDecodeError):
+            return ('UNKNOWN', 'Error running `hadoop health` command')
+
+        status = health_data.get('status', 'UNKNOWN')
+        message = health_data.get('message', 'No message returned')
+
+        if status == 'GOOD':
+            return ('OK', f'Hadoop {self.version} is healthy: {message}')
+        elif status == 'CONCERNING':
+            return ('WARNING', f'Hadoop {self.version} is concerning: {message}')
+        elif status == 'BAD':
+            return ('CRITICAL', f'Hadoop {self.version} is in a bad state: {message}')
+        else:
+            return ('UNKNOWN', f'Hadoop {self.version} is in an unknown state: {message}')
 
 
 if __name__ == '__main__':
-    status, message = check_hadoop()
+    hadoop_checker = HadoopChecker()
+    status, message = hadoop_checker.check_hadoop()
     print(f'{status} - {message}')
     sys.exit(status)
