279c2076d89c44c821afa675d5a90ca8360464c0
Thomas Vincent <thomasvincent@gmail.com>
2023-04-28 23:31:09 -0700

    Update check_http500.py

--- check_http500/check_http500.py
+++ check_http500/check_http500.py
@@ -1,24 +1,28 @@
-#!/usr/bin/env python3.5
+#!/usr/bin/env python3.6
 import os
 import sys
 import re
 import urllib.request
 import json
 import time
-import datetime
+from datetime import datetime
+
 
 def print_usage():
+    """Prints the usage information."""
     print("Usage:")
     print("hadoop.py -url=SOURCE_JSON_URL -t MAX_TIME_SINCE_LAST_UPDATE_IN_MINUTES (optional)")
     sys.exit(3)
 
-def main():
-    if len(sys.argv) < 2:
-        print_usage()
 
+def main():
+    """The main entry point of the program."""
+
+    # Parse the command line arguments.
+    args = sys.argv[1:]
     url_str = None
     time_str = None
-    for arg in sys.argv[1:]:
+    for arg in args:
         if arg.startswith("-url="):
             url_str = arg[5:]
         elif arg.startswith("-t"):
@@ -26,41 +30,49 @@ def main():
     if not url_str:
         print_usage()
 
+    # Check if the URL starts with "http://".
     if not url_str.startswith("http"):
         url_str = "http://" + url_str
 
-    try:
-        request = urllib.request.urlopen(url_str)
+    # Make a request to the URL.
+    with urllib.request.urlopen(url_str) as request:
         content = request.read()
-        jsondict = json.loads(content)
-        if jsondict["status"].lower() != "ok":
-            print(f'WARNING - Hadoop: {jsondict["message"]}')
+
+    # Decode the JSON response.
+    jsondict = json.loads(content)
+
+    # Check the status of the response.
+    if jsondict["status"].lower() != "ok":
+        print(f'WARNING - Hadoop: {jsondict["message"]}')
+        sys.exit(2)
+
+    # Iterate over the subcomponents.
+    for component in jsondict["subcomponents"]:
+        # Check the status of the subcomponent.
+        if component["status"].lower() != "ok":
+            print(f'WARNING - component "{component["name"]}" has status "{component["status"]}" and message: {component["message"]}')
             sys.exit(2)
 
-        subcomponents_lst = jsondict["subcomponents"]
-        for component in subcomponents_lst:
-            if component["status"].lower() != "ok":
-                print(f'WARNING - component "{component["name"]}" has status "{component["status"]}" and message: {component["message"]}')
-                sys.exit(2)
-            if time_str:
-                time_from_json = time.strptime(component["updated"], "%Y-%m-%d %H:%M:%S")
-                time_now = time.localtime()
-                time_from_json = datetime.datetime(*time_from_json[:6])
-                time_now = datetime.datetime(*time_now[:6])
-                time_delta = time_now - time_from_json
-                if time_delta.days > 0 or time_delta.seconds // 60 > int(time_str):
-                    print(f'WARNING - Component "{component["name"]}" has time since last update which is greater than {time_str} minutes')
-                    sys.exit(1)
+        # Check the time since the subcomponent was last updated.
+        if time_str:
+            time_from_json = datetime.strptime(component["updated"], "%Y-%m-%d %H:%M:%S")
+            time_now = datetime.now()
+            time_delta = time_now - time_from_json
+            if time_delta.days > 0 or time_delta.seconds // 60 > int(time_str):
+                print(f'WARNING - Component "{component["name"]}" has time since last update which is greater than {time_str} minutes')
+                sys.exit(1)
 
-        mem_component = subcomponents_lst[-1]
-        if mem_component["name"].lower() != "mem":
-            print("CRITICAL - Source URL has wrong content")
-            sys.exit(1)
-        else:
-            print(f"OK - mem: {mem_component['message'].replace('k','')}")
-    except Exception as e:
-        print(f'CRITICAL - {str(e)}')
-        sys.exit(2)
+    # Get the memory component.
+    mem_component = jsondict["subcomponents"][-1]
+
+    # Check the name of the memory component.
+    if mem_component["name"].lower() != "mem":
+        print("CRITICAL - Source URL has wrong content")
+        sys.exit(1)
+
+    # Print the memory usage.
+    print(f"OK - mem: {mem_component['message'].replace('k','')}")
+
 
 if __name__ == "__main__":
     main()
