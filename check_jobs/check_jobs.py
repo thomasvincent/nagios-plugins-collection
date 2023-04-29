39542f2b3475b6673c5bf86d11883e7a2ada33fe
Thomas Vincent <thomasvincent@gmail.com>
2023-04-28 23:10:38 -0700

    Update check_jobs.py
    refactored to use 
    his script uses the following features of Python 3:
    
    The typing module to annotate the types of variables and functions.
    The f-string syntax to format strings.
    The with statement to ensure that resources are closed properly.
    The dict.get() method to get the value of a key in a dictionary, or a default value if the key does not exist.
    The str.replace() method to replace a substring in a string.
    The sys.exit() function to exit the program with a given status code.

--- check_jobs/check_jobs.py
+++ check_jobs/check_jobs.py
@@ -1,4 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+
+from typing import Optional
 
 import os
 import sys
@@ -8,63 +10,82 @@ import json
 import time
 import datetime
 
-DEBUG_Flag = False
 
-def help():
-    print("Usage:")
-    print("check_jobs.py -url=JSON_URL")
-    sys.exit(3)
+def check_jobs(url: str, debug: bool = False) -> None:
+    """Check the status of jobs at the given URL.
 
-if len(sys.argv) == 2:
+    Args:
+        url: The URL of the job list.
+        debug: Whether to print debug messages.
+    """
+
+    # Check the number of arguments.
+    if len(sys.argv) != 2:
+        print("Usage:")
+        print("check_jobs.py -url=JSON_URL")
+        sys.exit(3)
+
+    # Get the URL and protocol.
     args_str = sys.argv[1]
     args_str = args_str.lower()
     if "-url=" in args_str:
+        url_arg = args_str.replace("http://", "").replace("-url=", "").replace("https://", "")
         proto = "http://"
         if "http://" in sys.argv[1]:
             proto = "http://"
         else:
             if "https://" in sys.argv[1]:
                 proto = "https://"
-
-        url_arg = sys.argv[1].replace("http://", "").replace("-url=", "").replace("https://", "")
         clean_url = f"{proto}{url_arg}"
-        if DEBUG_Flag:
-            print(f"Got url proto: {proto}")
-            print(f"Got clean url: {url_arg}")
-            print(f"Constructed url: {clean_url}")
+    else:
+        print("UNKNOWN - Wrong arguments!")
+        sys.exit(3)
 
-        try:
-            request = urllib.request.urlopen(clean_url)
+    # Check the URL.
+    if not clean_url.startswith(("http://", "https://")):
+        print("UNKNOWN - Invalid URL:", clean_url)
+        sys.exit(3)
+
+    # Make the request.
+    try:
+        with urllib.request.urlopen(clean_url) as request:
             content = request.read()
-            if DEBUG_Flag:
-                print(content)
+    except (urllib.error.URLError, urllib.error.HTTPError) as e:
+        print(f"UNKNOWN - URL/HTTP Error: {e}")
+        sys.exit(3)
 
-            json_dict = json.loads(content)
-            if DEBUG_Flag:
-                print(repr(json_dict))
-            status = json_dict["status"].lower()
-            if status == "ok":
-                for component in json_dict["components"]:
-                    component_status = component["status"].lower()
-                    component_message = ""
-                    if component["message"]:
-                        component_message = component["message"].replace("\n", "; ")
-                    else:
-                        component_message = " "
+    # Parse the JSON response.
+    json_dict = json.loads(content)
 
-                    if component_status != "ok":
-                        print(f"WARNING - Component {component['name']} has status {component_status.upper()}, message: {component_message}")
-                        sys.exit(1)
-
-                print("SUCCESS - Root status is OK. All components has status OK.")
-                sys.exit(0)
+    # Check the status.
+    status = json_dict["status"].lower()
+    if status == "ok":
+        for component in json_dict["components"]:
+            component_status = component["status"].lower()
+            component_message = ""
+            if component["message"]:
+                component_message = component["message"].replace("\n", "; ")
             else:
-                print(f"WARNING - Root status {json_dict['title'].upper()} is {status.upper()}. Message: {json_dict['message'].replace('\n','; ')}")
+                component_message = " "
+
+            if component_status != "ok":
+                print(f"WARNING - Component {component['name']} has status {component_status.upper()}, message: {component_message}")
                 sys.exit(1)
 
-        except (urllib.error.URLError, urllib.error.HTTPError) as e:
-            print(f"UNKNOWN - URL/HTTP Error: {e}")
-            sys.exit(3)
-else:
-    print("UNKNOWN - Wrong arguments!")
-    sys.exit(3)
+        print("SUCCESS - Root status is OK. All components has status OK.")
+        sys.exit(0)
+    else:
+        print(f"WARNING - Root status {json_dict['title'].upper()} is {status.upper()}. Message: {json_dict['message'].replace('\n','; ')}")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    # Parse the command-line arguments.
+    url = sys.argv[1]
+    debug = False
+    if len(sys.argv) == 3:
+        if sys.argv[2] == "-debug":
+            debug = True
+
+    # Check the jobs.
+    check_jobs(url, debug)
