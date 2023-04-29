a17a2ccee25d26c48f1238ed97628e79fda2fbf9
Thomas Vincent <thomasvincent@gmail.com>
2023-04-28 23:23:32 -0700

    Update etl.py
    **Refactored code for improved algorithm**
    
    This commit refactors the code for improved algorithm. The following changes were made:
    
    * A single function is used to check the status of a component.
    * A dictionary is used to store the component status and last updated time.
    * A for loop is used to iterate over the components and check their status.
    * A conditional statement is used to determine the severity of the error.

--- check_etl/etl.py
+++ check_etl/etl.py
@@ -1,67 +1,52 @@
 import argparse
 import json
-import urllib.request
-import urllib.error
+import requests
 import datetime
-from typing import Tuple
+from logging import getLogger
 
-def parse_args() -> argparse.Namespace:
-    """
-    Parses command line arguments and returns them as a Namespace object
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-url", required=True, help="URL of the hostname")
-    parser.add_argument("-component", required=True, help="Name of the component")
-    parser.add_argument("-t", required=True, type=int, help="Max time since last update in minutes")
-    return parser.parse_args()
+logger = getLogger(__name__)
 
-def component_checker(url: str, component: str) -> Tuple[str, Union[datetime.timedelta, str]]:
+def get_component_status(url, component):
     """
     Check the status of a component by querying the JSON API.
 
     :param url: URL of the hostname
     :param component: Name of the component
-    :return: a tuple of (status, result) where status is one of ('ok', 'error')
-    and result is a timedelta object representing the time since last update for 'ok' status 
-    or a string representing the error message for 'error' status
+    :return: a tuple of (status, updated) where status is one of ('ok', 'error')
+    and updated is the time the component was last updated
     """
     try:
-        request = urllib.request.urlopen(f"http://{url}/api/component/{component}")
-        content = request.read().decode()
-        json_dict = json.loads(content)
-        status = json_dict["status"].lower()
-        if status == "ok":
-            time_from_json = datetime.datetime.strptime
-            time_from_json = datetime.datetime.strptime(json_dict["updated"], "%Y-%m-%d %H:%M:%S")
-            time_now = datetime.datetime.now()
-            time_delta = time_now - time_from_json
-            return (status, time_delta)
+        response = requests.get(f"http://{url}/api/component/{component}")
+        if response.status_code == 200:
+            json_dict = response.json()
+            status = json_dict["status"].lower()
+            updated = json_dict["updated"]
+            return (status, updated)
         else:
-            return (status, json_dict["message"])
-    except urllib.error.URLError as e:
-        return ("error", str(e))
-    except urllib.error.HTTPError as e:
-        return ("error", str(e))
+            logger.error(f"Error fetching component status: {response.status_code}")
+            return ("error", None)
+    except requests.exceptions.RequestException as e:
+        logger.error(f"Error fetching component status: {e}")
+        return ("error", None)
 
 def main() -> int:
     """
     The main function of the script.
     """
     args = parse_args()
-    status, result = component_checker(args.url, args.component)
-    if status == "ok":
-        if result.total_seconds() / 60 > args.t:
-            print(f"WARNING - Time since last update is greater than {args.t} minutes")
-            return 1
+    component_statuses = {}
+    for component in args.components:
+        status, updated = get_component_status(args.url, component)
+        component_statuses[component] = (status, updated)
+    for component, (status, updated) in component_statuses.items():
+        if status == "ok":
+            if (updated is not None) and (datetime.datetime.now() - updated).total_seconds() / 60 > args.t:
+                logger.warning(f"WARNING - Component {component} has not been updated in more than {args.t} minutes")
+            else:
+                logger.info(f"OK - Component {component} has been updated within the last {args.t} minutes")
         else:
-            print(f"OK - Time since last update is less than {args.t} minutes")
-            return 0
-    elif status == "error":
-        print(f"CRITICAL - {result}")
-        return 2
-    else:
-        print(f"CRITICAL - {result}")
-        return 2
+            logger.critical(f"CRITICAL - Component {component} is in error state")
+    return 0
 
 if __name__ == "__main__":
     sys.exit(main())
