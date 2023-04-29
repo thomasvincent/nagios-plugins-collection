664f039df0d0a9944c76ff13655e185f10117217
Thomas Vincent <thomasvincent@gmail.com>
2023-04-28 23:16:35 -0700

    Create check_status_test.py

---
+++ advanced_website_status_check/check_status_test.py
@@ -0,0 +1,42 @@
+import pytest
+
+from check_status import check_status
+
+
+@pytest.mark.parametrize("url, expected_status_code, test_description", [
+    (
+        "https://www.google.com",
+        200,
+        "A successful request.",
+    ),
+    (
+        "https://www.google.com.invalid",
+        404,
+        "A request to a non-existent URL.",
+    ),
+    (
+        "https://www.google.com/does-not-exist",
+        404,
+        "A request to a path that does not exist.",
+    ),
+    (
+        "https://www.google.com/?timeout=1",
+        408,
+        "A request that times out after 1 second.",
+    ),
+    (
+        "https://www.google.com/?timeout=2",
+        408,
+        "A request that times out after 2 seconds.",
+    ),
+])
+def test_check_status(url, expected_status_code, test_description):
+    # Arrange
+    actual_status_code = check_status(url)
+    
+    # Assert
+    assert actual_status_code == expected_status_code, test_description
+
+
+if __name__ == "__main__":
+    pytest.main()
