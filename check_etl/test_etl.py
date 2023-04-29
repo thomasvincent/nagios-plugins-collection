738a8f3ab633d35d8718ddf277275b2c58fb9789
Thomas Vincent <thomasvincent@gmail.com>
2023-04-28 23:27:22 -0700

    Update test_etl.py

--- check_etl/test_etl.py
+++ check_etl/test_etl.py
@@ -1,4 +1,4 @@
-import unittest
+import pytest
 from unittest.mock import patch
 import argparse
 import json
@@ -7,24 +7,22 @@ import urllib.error
 import datetime
 from component_checker import main, parse_args, component_checker
 
-class TestMain(unittest.TestCase):
-    def test_main_ok(self):
-        with patch('component_checker.component_checker') as mocked_component_checker:
-            mocked_component_checker.return_value = ('ok', datetime.timedelta(minutes=10))
-            args = parse_args(['-url=example.com', '-component=component1', '-t=30'])
-            result = main(args)
-            self.assertEqual(result, 0)
 
-    def test_main_warning(self):
-        with patch('component_checker.component_checker') as mocked_component_checker:
-            mocked_component_checker.return_value = ('ok', datetime.timedelta(minutes=40))
-            args = parse_args(['-url=example.com', '-component=component1', '-t=30'])
-            result = main(args)
-            self.assertEqual(result, 1)
+@pytest.mark.parametrize("status, expected_result, expected_log_message", [
+    ("ok", 0, "OK - Component component1 has been updated within the last 30 minutes"),
+    ("warning", 1, "WARNING - Component component1 has not been updated in more than 30 minutes"),
+    ("error", 2, "CRITICAL - Component component1 is in error state"),
+])
+def test_main(status, expected_result, expected_log_message):
+    with patch("component_checker.component_checker") as mocked_component_checker:
+        mocked_component_checker.return_value = (status, datetime.timedelta(minutes=10))
+        args = parse_args(["-url=example.com", "-component=component1", "-t=30"])
+        result = main(args)
+        assert result == expected_result
+        assert mocked_component_checker.called
+        assert mocked_component_checker.call_args == (args.url, args.component)
+        assert main.logger.info.call_args == (expected_log_message,)
 
-    def test_main_error(self):
-        with patch('component_checker.component_checker') as mocked_component_checker:
-            mocked_component_checker.return_value = ('error', 'something went wrong')
-            args = parse_args(['-url=example.com', '-component=component1', '-t=30'])
-            result = main(args)
-            self.assertEqual(result, 2)
+
+if __name__ == "__main__":
+    pytest.main()
